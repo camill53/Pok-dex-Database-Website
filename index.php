7e1016244bf8fc7c2b6f82c59a532a1f7822d8ef
Anoosh K <anooshkapadia@gmail.com>
2022-04-24 10:31:49 -0700

    spaghetti code

--- index.php
+++ index.php
@@ -18,20 +18,23 @@
                 <div class="screenOutlines">
                     <div class="promptScreen">
                     <form action="index.php" method="GET">
-                        <label for="pokemon_name" class="label"> Pokemon Name:</label>
-                        <input type="text" class="textInput" name="pokemon_name">
-                        
-                        <label for="pokedex_number"> Pokedex Number:</label>
-                        <input type="number" class="textInput" name="pokedex_number">
-                
-                        <label for="pokemon_generation"> Pokemon Generation:</label>
-                        <input type="number" class="textInput" name="pokemon_generation">
-                    
-                        <label for="pokemon_type"> Pokemon Type:</label>
-                        <input type="text" class="textInput" name="pokemon_type">
-                
-                        <label for="region"> Region Origin :</label>
-                        <input type="text" class="textInput" name="region">
+                        <label for="pokemon_name"> Enter Pokemon Name:</label>
+						<input type="text" name="pokemon_name">
+					  
+						<label for="pokedex_number"> Enter Pokedex Number:</label>
+						<input type="number" name="pokedex_number">
+				
+						<label for="pokemon_generation"> Enter Pokemon Generation:</label>
+						<input type="number" name="pokemon_generation">
+				  
+						<label for="pokemon_type"> Enter Pokemon Type:</label>
+						<input type="text" name="pokemon_type">
+			  
+						<label for="region"> Enter Region Origin :</label>
+						<input type="text" name="region">
+						
+						<label for="pokemon_ability"> Enter Pokemon Ability :</label>
+						<input type="text" name="pokemon_ability">
 
                         <input type="submit" class="submit" style="margin-left: 40%" value="Submit">
                         </form>
@@ -47,76 +50,92 @@
                              // this is just an if statement to check if the DB connection is successful -->
                             if(!$db) {
                             echo "Unable to connect to Pokedex. Please contact Professor Oak.\n";
-                            } /*else {
-                                echo "What Pokemon are you looking for?<br>";
-                            }*/
-
-                             // these next lines run queries for pokemon name, pokedex number, type, generation, and region
-                            // the queries use joins within them and store the output in the corresponding php variables
-                            // I used the forms above with the $_GET command to add the user input to the query
-                            // the input corresponds to the name in the form above
-                            // I kept the queries simple so that the user could only search for pokemon by name or pokedex number
-                            // User can also search by generation, region and type -->
-
-                            $name = pg_query($db, "SELECT name, pokemon.pokedexnum, regionname, region.generation, normcolor, shinycolor FROM Pokemon join fromregion on Pokemon.pokedexnum = fromregion.pokedexnum join region on fromregion.generation = region.generation WHERE Pokemon.name = '$_GET[pokemon_name]'");
-                            echo "<br>";
-
-                            $type = pg_query($db, "select name, typename from pokemon join of on pokemon.pokedexnum = of.pokedexnum where typename = '$_GET[pokemon_type]'
-                            ");
-
-                            $gen = pg_query($db, "SELECT name, pokemon.pokedexnum, regionname, region.generation, normcolor, shinycolor FROM Pokemon join fromregion on Pokemon.pokedexnum = fromregion.pokedexnum join region on fromregion.generation = region.generation WHERE fromregion.generation = '$_GET[pokemon_generation]'");
-                            echo "<br>";
-
-                            $pokenum = pg_query($db, "SELECT name, pokemon.pokedexnum, regionname, region.generation, normcolor, shinycolor FROM Pokemon join fromregion on Pokemon.pokedexnum = fromregion.pokedexnum join region on fromregion.generation = region.generation WHERE Pokemon.Pokedexnum = '$_GET[pokedex_number]'");
-
-                            $region = pg_query($db, "SELECT name, pokemon.pokedexnum, regionname, region.generation, normcolor, shinycolor FROM Pokemon join fromregion on Pokemon.pokedexnum = fromregion.pokedexnum join region on fromregion.generation = region.generation WHERE region.regionname = '$_GET[region]'");
-
-                            // Below I just used multiple if statements to check which variable had the user input
-                            // Only one form can be used at a time to make the website simple
-                            // within each if statement there is a while loop and $row stores the results of the query
-                            // After that echo is used to print each row
-                            // Each row works like an array, so you just use the indexes to print the data from each column  -->
-
-                            if($name) {
-                                while($row = pg_fetch_row($name)) {
-
-                                    echo "Pokemon Name: $row[0]  Pokedex Number: $row[1]  Region: $row[2]  Generation: $row[3]  Normal Color: $row[4]  Shiny Color: $row[5] <br>";
-                                    echo "<br />\n";
-                                } 
                             } 
 
-                            if($type) {
-                                while($row = pg_fetch_row($type)) {
+                            $checkothers = true;
+							$checkothersTwo = true;
+							$checkregion = false;
+							$typeExists = false;
+							$FromSubString = "";
+							$WhereSubString = "";
+							
+							// If pokemon name is entered, then ignore all other inputs
+							if($_GET[pokemon_name] != ""){
+								$req =  pg_query($db, "SELECT name, pokemon.pokedexnum, regionname, region.generation, normcolor, shinycolor FROM Pokemon NATURAL JOIN fromregion NATURAL JOIN region WHERE Pokemon.name = '$_GET[pokemon_name]'");
+								echo "<br>";
+								$checkothers = false;
+								$checkothersTwo = false;
+								if(!$req && $_GET[pokedex_number] != "")
+									$checkothersTwo = true;
+								else if(!$req && $_GET[pokedex_number] == "")
+									$checkothers = true;	
+							}
 
-                                    echo "Pokemon Name: $row[0]  Type: $row[1] <br>";
-                                    echo "<br />\n";
-                                } 
-                            }
+							if($_GET[pokedex_number] == "")
+								$checkothersTwo = false;
 
+							if($_GET[pokedex_number] != "" && checkothersTwo){
+								$req = pg_query($db, "SELECT name, pokemon.pokedexnum, regionname, region.generation, normcolor, shinycolor FROM Pokemon NATURAL JOIN fromregion NATURAL JOIN region WHERE Pokemon.Pokedexnum = $_GET[pokedex_number]");
+								$checkothers = false;
+								//echo "SELECT name, pokemon.pokedexnum, regionname, region.generation, normcolor, shinycolor FROM Pokemon NATURAL JOIN fromregion NATURAL JOIN region WHERE Pokemon.Pokedexnum = $_GET[pokedex_number]";
+								if(!$req)
+									$checkothers = true;
+								
+							}
 
-                            if($pokenum) {
-                                while($row = pg_fetch_row($pokenum)) {
-
-                                    echo "Pokemon Name: $row[0]  Pokedex Number: $row[1]  Region: $row[2]  Generation: $row[3]  Normal Color: $row[4]  Shiny Color: $row[5] <br>";
-                                    echo "<br />\n";
-                                } 
-                            } 
-
-                            if($gen) {
-                                while($row = pg_fetch_row($gen)) {
-
-                                    echo "Pokemon Name: $row[0]  Pokedex Number: $row[1]  Region: $row[2]  Generation: $row[3]  Normal Color: $row[4]  Shiny Color: $row[5] <br>";
-                                    echo "<br />\n";
-                                } 
-                            } 
-
-                            if($region) {
-                                while($row = pg_fetch_row($region)) {
-
-                                    echo "Pokemon Name: $row[0]  Pokedex Number: $row[1]  Region: $row[2]  Generation: $row[3]  Normal Color: $row[4]  Shiny Color: $row[5] <br>";
-                                    echo "<br />\n";
-                                } 
-                            } 
+							if($_GET[pokemon_type] != ""){
+								$FromSubString .= " NATURAL JOIN of";
+								$WhereSubString .= "typename = '$_GET[pokemon_type]'";
+								$typeExists = true;
+							}
+							if($_GET[pokemon_generation] != ""){
+								$checkregion = true;
+								if($_GET[pokemon_generation] <= 8 && $_GET[pokemon_generation] > 0){
+									if($typeExists)
+										$WhereSubString .= " AND ";
+									$WhereSubString .= "fromregion.generation = '$_GET[pokemon_generation]'";
+									$checkregion = false;
+									$typeExists = true;
+								}
+							}
+							if($_GET[pokemon_ability] != ""){
+								$FromSubString .= " NATURAL JOIN has";
+								if($typeExists)
+									$WhereSubString .= " AND ";
+								$WhereSubString .= "has.abilityname = '$_GET[pokemon_ability]'";
+								$typeExists = true;
+								
+							}
+							if($_GET[region] != "" && $checkregion || $_GET[pokemon_generation] == ""){
+								if($typeExists)
+									$WhereSubString .= " AND ";
+								$WhereSubString .= "region.regionname = '$_GET[region]'";
+							}
+							
+							if($checkothers){
+								$req = pg_query($db, "SELECT name, pokemon.pokedexnum, regionname FROM pokemon NATURAL JOIN fromregion NATURAL JOIN region $FromSubString WHERE $WhereSubString");
+								// echo "SELECT name, pokemon.pokedexnum, regionname FROM pokemon NATURAL JOIN fromregion NATURAL JOIN region$FromSubString WHERE $WhereSubString";
+							}						
+							if($req){
+								if(!$checkothers || $checkothersTwo){
+									while($row = pg_fetch_row($req)) {
+										echo "xD";
+										echo "Pokemon Name: $row[0]<br>  Pokedex Number: $row[1]<br>  Region: $row[2]<br> Generation: $row[3]<br>  Normal Color: $row[4]<br>  Shiny Color: $row[5] <br>";
+										echo "<br />\n";
+									} 
+								}
+								else{
+									while($row = pg_fetch_row($req)) {
+										echo "Pokemon Name: $row[0]\n  Pokedex Number: $row[1]\n  Region: $row[2]  <br>";
+										echo "<br />\n";
+									} 
+								}
+							}
+							else{
+								echo "No Pokemon Found";
+							}
+							if($_GET[pokemon_generation] == 3 || $_GET[region] == "Hoenn")
+								echo "Pokemon from the Hoenn region went extinct due to rising sea levels";
                         ?>
                             
                     </div>
--- site.php
+++ site.php
@@ -116,76 +116,92 @@ $db = pg_connect("host=localhost port=5432 dbname=Pokemon user=postgres password
 // this is just an if statement to check if the DB connection is successful
 if(!$db) {
    echo "Error: Unable to open database\n";
-} else {
-    echo "Opened database succesfully <br>";
+} 
+
+$checkothers = true;
+$checkothersTwo = true;
+$checkregion = false;
+$typeExists = false;
+$FromSubString = "";
+$WhereSubString = "";
+
+// If pokemon name is entered, then ignore all other inputs
+if($_GET[pokemon_name] != ""){
+	$req =  pg_query($db, "SELECT name, pokemon.pokedexnum, regionname, region.generation, normcolor, shinycolor FROM Pokemon NATURAL JOIN fromregion NATURAL JOIN region WHERE Pokemon.name = '$_GET[pokemon_name]'");
+	echo "<br>";
+	$checkothers = false;
+	$checkothersTwo = false;
+	if(!$req && $_GET[pokedex_number] != "")
+		$checkothersTwo = true;
+	else if(!$req && $_GET[pokedex_number] == "")
+		$checkothers = true;	
 }
 
-// these next lines run queries for pokemon name, pokedex number, type, generation, and region
-// the queries use joins within them and store the output in the corresponding php variables
-// I used the forms above with the $_GET command to add the user input to the query
-// the input corresponds to the name in the form above
-// I kept the queries simple so that the user could only search for pokemon by name or pokedex number
-// User can also search by generation, region and type
+if($_GET[pokedex_number] == "")
+	$checkothersTwo = false;
 
-$name = pg_query($db, "SELECT name, pokemon.pokedexnum, regionname, region.generation, normcolor, shinycolor FROM Pokemon join fromregion on Pokemon.pokedexnum = fromregion.pokedexnum join region on fromregion.generation = region.generation WHERE Pokemon.name = '$_GET[pokemon_name]'");
-echo "<br>";
-
-$type = pg_query($db, "select name, typename from pokemon join of on pokemon.pokedexnum = of.pokedexnum where typename = '$_GET[pokemon_type]'
-");
-
-$gen = pg_query($db, "SELECT name, pokemon.pokedexnum, regionname, region.generation, normcolor, shinycolor FROM Pokemon join fromregion on Pokemon.pokedexnum = fromregion.pokedexnum join region on fromregion.generation = region.generation WHERE fromregion.generation = '$_GET[pokemon_generation]'");
-echo "<br>";
-
-$pokenum = pg_query($db, "SELECT name, pokemon.pokedexnum, regionname, region.generation, normcolor, shinycolor FROM Pokemon join fromregion on Pokemon.pokedexnum = fromregion.pokedexnum join region on fromregion.generation = region.generation WHERE Pokemon.Pokedexnum = '$_GET[pokedex_number]'");
-
-$region = pg_query($db, "SELECT name, pokemon.pokedexnum, regionname, region.generation, normcolor, shinycolor FROM Pokemon join fromregion on Pokemon.pokedexnum = fromregion.pokedexnum join region on fromregion.generation = region.generation WHERE region.regionname = '$_GET[region]'");
-
-// Below I just used multiple if statements to check which variable had the user input
-// Only one form can be used at a time to make the website simple
-// within each if statement there is a while loop and $row stores the results of the query
-// After that echo is used to print each row
-// Each row works like an array, so you just use the indexes to print the data from each column 
-
-if($name) {
-    while($row = pg_fetch_row($name)) {
-
-        echo "Pokemon Name: $row[0]  Pokedex Number: $row[1]  Region: $row[2]  Generation: $row[3]  Normal Color: $row[4]  Shiny Color: $row[5] <br>";
-        echo "<br />\n";
-    } 
-} 
-
-if($type) {
-    while($row = pg_fetch_row($type)) {
-
-        echo "Pokemon Name: $row[0]  Type: $row[1] <br>";
-        echo "<br />\n";
-    } 
+if($_GET[pokedex_number] != "" && checkothersTwo){
+	$req = pg_query($db, "SELECT name, pokemon.pokedexnum, regionname, region.generation, normcolor, shinycolor FROM Pokemon NATURAL JOIN fromregion NATURAL JOIN region WHERE Pokemon.Pokedexnum = $_GET[pokedex_number]");
+	$checkothers = false;
+	//echo "SELECT name, pokemon.pokedexnum, regionname, region.generation, normcolor, shinycolor FROM Pokemon NATURAL JOIN fromregion NATURAL JOIN region WHERE Pokemon.Pokedexnum = $_GET[pokedex_number]";
+	if(!$req)
+		$checkothers = true;
+	
 }
 
+if($_GET[pokemon_type] != ""){
+	$FromSubString .= " NATURAL JOIN of";
+	$WhereSubString .= "typename = '$_GET[pokemon_type]'";
+	$typeExists = true;
+}
+if($_GET[pokemon_generation] != ""){
+	$checkregion = true;
+	if($_GET[pokemon_generation] <= 8 && $_GET[pokemon_generation] > 0){
+		if($typeExists)
+			$WhereSubString .= " AND ";
+		$WhereSubString .= "fromregion.generation = '$_GET[pokemon_generation]'";
+		$checkregion = false;
+		$typeExists = true;
+	}
+}
+if($_GET[pokemon_ability] != ""){
+	$FromSubString .= " NATURAL JOIN has";
+	if($typeExists)
+		$WhereSubString .= " AND ";
+	$WhereSubString .= "has.abilityname = '$_GET[pokemon_ability]'";
+	$typeExists = true;
+	
+}
+if($_GET[region] != "" && $checkregion || $_GET[pokemon_generation] == ""){
+	if($typeExists)
+		$WhereSubString .= " AND ";
+	$WhereSubString .= "region.regionname = '$_GET[region]'";
+}
 
-if($pokenum) {
-    while($row = pg_fetch_row($pokenum)) {
-
-        echo "Pokemon Name: $row[0]  Pokedex Number: $row[1]  Region: $row[2]  Generation: $row[3]  Normal Color: $row[4]  Shiny Color: $row[5] <br>";
-        echo "<br />\n";
-    } 
-} 
-
-if($gen) {
-    while($row = pg_fetch_row($gen)) {
-
-        echo "Pokemon Name: $row[0]  Pokedex Number: $row[1]  Region: $row[2]  Generation: $row[3]  Normal Color: $row[4]  Shiny Color: $row[5] <br>";
-        echo "<br />\n";
-    } 
-} 
-
-if($region) {
-    while($row = pg_fetch_row($region)) {
-
-        echo "Pokemon Name: $row[0]  Pokedex Number: $row[1]  Region: $row[2]  Generation: $row[3]  Normal Color: $row[4]  Shiny Color: $row[5] <br>";
-        echo "<br />\n";
-    } 
-} 
+if($checkothers){
+	$req = pg_query($db, "SELECT name, pokemon.pokedexnum, regionname FROM pokemon NATURAL JOIN fromregion NATURAL JOIN region $FromSubString WHERE $WhereSubString");
+	// echo "SELECT name, pokemon.pokedexnum, regionname FROM pokemon NATURAL JOIN fromregion NATURAL JOIN region$FromSubString WHERE $WhereSubString";
+}						
+if($req){
+	if(!$checkothers || $checkothersTwo){
+		while($row = pg_fetch_row($req)) {
+			echo "xD";
+			echo "Pokemon Name: $row[0]<br>  Pokedex Number: $row[1]<br>  Region: $row[2]<br> Generation: $row[3]<br>  Normal Color: $row[4]<br>  Shiny Color: $row[5] <br>";
+			echo "<br />\n";
+		} 
+	}
+	else{
+		while($row = pg_fetch_row($req)) {
+			echo "Pokemon Name: $row[0]\n  Pokedex Number: $row[1]\n  Region: $row[2]  <br>";
+			echo "<br />\n";
+		} 
+	}
+}
+else{
+	echo "No Pokemon Found";
+}
+if($_GET[pokemon_generation] == 3 || $_GET[region] == "Hoenn")
+	echo "Pokemon from the Hoenn region went extinct due to rising sea levels";
 
 ?>
 
